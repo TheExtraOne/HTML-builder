37f5664ec090a8f0d0b9c1b3c7f5fa54a63e04e7
TheExtraOne <katya_mihasewa@mail.ru>
2022-11-02 21:25:03 +0300

    feat: add 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+const fsPromises = fs.promises;
 
+fsPromises.mkdir( path.join(__dirname, 'files-copy'), {recursive: true} )
+.then(function() {
+    fs.promises.readdir(path.join(__dirname, 'files-copy'))
+    .then(filenames => {
+        for (let file of filenames) {
+            fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+                if(err) throw err; // не удалось удалить файл
+            });
+        }
+    })
+
+    fs.promises.readdir(path.join(__dirname, 'files'))
+
+    // If promise resolved and
+    // data are fetched
+    .then(filenames => {
+        for (let file of filenames) {
+            fs.writeFile(
+                path.join(__dirname, 'files-copy', file),
+                '',
+                (err) => {
+                    if (err) throw err;
+                }
+            );
+
+            fs.copyFile(path.join(__dirname, 'files',file), path.join(__dirname, 'files-copy', file), (err) => {
+                if (err) {
+                    console.log("Error Found:", err);
+                }
+                else {
+                    console.log('Copied successfully');
+                }
+            });
+        }
+    })
+    // If promise is rejected
+    .catch(err => {
+        console.log(err)
+    })
+}).catch(function() {
+    console.log('failed to create directory');
+});
