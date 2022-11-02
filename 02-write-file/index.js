2aa1b94eb3c7af02330957e62f68bf8996f0eda1
TheExtraOne <katya_mihasewa@mail.ru>
2022-11-02 15:15:14 +0300

    feat:start 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile(
+    path.join(__dirname, 'usertext.txt'),
+    '',
+    (err) => {
+        if (err) throw err;
+        console.log('Hello, please, enter your text');
+    }
+);
+const process = require('process');
+const { stdin } = process;
+const output = fs.createWriteStream(path.join(__dirname, 'usertext.txt'));
+
+stdin.setEncoding('utf8');
+stdin.on('data', (data) => {
+    if (data === 'exit') {
+        process.exit();
+    }
+    console.log(data == 'exit');
+    output.write(data);
+    console.log('You add something important, please, continue');
+});
+
+process.on('exit', () => console.log('Have a nice day'));
+
